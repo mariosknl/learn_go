2ad9a973469770b549f6f7b5f3637a3c24c2b5a0
Marios Kanellopoulos <mariosknl@outlook.com>
2025-08-21 19:22:36 +0300

    Add concurrency package with website checking functionality and benchmarks
    - Implemented CheckWebsites function to concurrently check the status of multiple URLs using goroutines.
    - Added concurrency_benchmark_test.go to benchmark the performance of website checking.
    - Created concurrency_test.go to test the CheckWebsites function with various URLs, ensuring correct functionality through table-driven tests.
    - Introduced mock functions for testing purposes, enhancing test coverage and reliability.

---
+++ mocking/mock.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+type DefaultSleeper struct {}
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
+	}
+
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+	}
+
+	fmt.Fprintf(out, finalWord)
+}
+
+func main() {
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
